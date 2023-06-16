85499b7ecf966445e564612840aa0b08db28a012
Tate Thomas <106632630+TateThomas@users.noreply.github.com>
2023-06-15 19:40:58 -0600

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,48 @@
+let appFooter = `
+    <div id="footer">
+
+        <div id="nav">
+            <div id="site-nav">
+                <h2>Navigate</h2>
+                <ul>
+                    <li><a href="https://brynelle.com/packages">Packages</a></li>
+                    <li><a href="https://brynelle.com/travel">Travel</a></li>
+                    <li><a href="https://brynelle.com/gallery">Gallery</a></li>
+                    <li><a href="https://brynelle.com/contacts">Contact</a></li>
+                    <li><a href="https://brynelle.com/about_me">About me</a></li>
+                    <li><a href="https://brynelle.com/more">More</a></li>
+                </ul>
+            </div>
+
+            <div id="book">
+                <h2>Book Here</h2>
+                <ul>
+                    <li><a>Link</a></li>
+                    <li><a>Help</a></li>
+                </ul>
+            </div>
+
+            <div id="socials">
+                <h2>Follow Me</h2>
+                <ul>
+                    <li><a href="https://www.instagram.com/brynellephoto/"><img src="images/socials/insta.png"></a></li>
+                    <li><a><img src="images/socials/facebook.png"></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="contact-info">
+            <ul>
+                <li>Brynelle Smith</li>
+                <li>•</li>
+                <li><a href="tel:3853898089">385-389-8089</a></li>
+                <li>•</li>
+                <li><a href="mailto:brynelle@hotmail.com">brynelle@hotmail.com</a></li>
+            </ul>
+        </div>
+
+        <h3>© Brynelle Photo 2023</h3>
+
+    </div>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
---
+++ header.js
@@ -0,0 +1,27 @@
+let appHeader = `
+    <div id="header">
+
+        <a href="https://brynelle.com">
+            <h1>Brynelle Photo</h1>
+        </a>
+
+        <div id="burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <nav id="links">
+            <ul>
+                <li><a href="https://brynelle.com/packages">Packages</a></li>
+                <li><a href="https://brynelle.com/travel">Travel</a></li>
+                <li><a href="https://brynelle.com/gallery">Gallery</a></li>
+                <li><a href="https://brynelle.com/contact">Contact</a></li>
+                <li><a href="https://brynelle.com/about_me">About Me</a></li>
+                <li><a href="https://brynelle.com/more">More</a></li>
+            </ul>
+        </nav>
+
+    </div>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
