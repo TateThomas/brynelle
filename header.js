a90b78378bbee1a9edcfac5e157e86cc210d1b91
Tate Thomas <106632630+TateThomas@users.noreply.github.com>
2023-06-18 01:58:52 -0600

    Changed header links, simplified transition
    Changed what the header links were, added drop down menus for 2 links, and made it have smooth transitions. Simplified some complexity with the transition image to allow for easier changes in the future, utilizing CSS variables and calculations. Will probably remove the transition from the mid-page to allow for some whitespace though. I also think the image looks better up against the footer.

--- header.js
+++ header.js
@@ -15,12 +15,58 @@ let appHeader = `
 
         <nav id="links">
             <ul>
-                <li><a href="https://www.brynelle.com/packages">Packages</a></li>
-                <li><a href="https://www.brynelle.com/travel">Travel</a></li>
-                <li><a href="https://www.brynelle.com/gallery">Gallery</a></li>
-                <li><a href="https://www.brynelle.com/contact">Contact</a></li>
-                <li><a href="https://www.brynelle.com/about_me">About Me</a></li>
-                <li><a href="https://www.brynelle.com/more">More</a></li>
+                <li>
+                    <a href="https://www.brynelle.com/pricing">Pricing</a>
+                </li>
+                <li>
+                    <a href="https://www.brynelle.com/travel">Travel</a>
+                </li>
+                <li>
+                    <a href="https://www.brynelle.com/portfolio">Portfolio</a>
+                    <ul>
+                        <li>
+                            <a href="https://www.brynelle.com/portfolio/weddings">Weddings</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/portfolio/engagements">Engagements</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/portfolio/couples">Couples</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/portfolio/families">Families</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/portfolio/portraits">Portraits</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/portfolio/other">Other</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="https://www.brynelle.com/events">Events</a>
+                    <ul>
+                        <li>
+                            <a href="https://www.brynelle.com/events/mini_sessions">Mini Sessions</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/events/content_days">Content Days</a>
+                        </li>
+                        <li>
+                            <a href="https://www.brynelle.com/events/content_retreats">Content Retreats</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="https://www.brynelle.com/about_me">About Me</a>
+                </li>
+                <li>
+                    <a href="https://www.brynelle.com/contact">Contact</a>
+                </li>
+                <li>
+                    <a href="https://www.brynelle.com/help">Help</a>
+                </li>
             </ul>
         </nav>
 
