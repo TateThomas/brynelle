d416da7d982e99c7176a13928834b06efbddec55
Tate Thomas <106632630+TateThomas@users.noreply.github.com>
2023-07-23 05:46:27 -0600

    Started and completed Gallery Home page
    Finished page and also finished making it responsive. Minor design changes will probably happen in the future though.

---
+++ gallery.js
@@ -0,0 +1,38 @@
+
+function headerAnimation() {
+    
+    const header = document.getElementById("header");
+    const info = document.getElementById("top-page").querySelector(".info");
+    const iconBreak1 = document.getElementById("icons").getElementsByTagName("h1")[0];
+    const ig = document.getElementById("ig");
+    
+    let distanceFromInfo = info.getBoundingClientRect().y - header.clientHeight;
+    let distanceFromPhotos = iconBreak1.getBoundingClientRect().y + iconBreak1.clientHeight - header.clientHeight;
+    let distanceFromIg = ig.getBoundingClientRect().y - header.clientHeight;
+    
+    let extraDistance = 100;
+    
+    if ((distanceFromInfo - extraDistance) > 0) {
+        header.className = "hidden";
+    }
+    else if ((distanceFromPhotos > 0) && ((distanceFromInfo - extraDistance) <= 0)) {
+        header.className = "show";
+    }
+    else if (((distanceFromIg - extraDistance) > 0) && (distanceFromPhotos <= 0)) {
+        header.className = "hidden";
+    }
+    else {
+        header.className = "show";
+    }
+    
+}
+
+
+function scrollFunctions() {
+    
+    headerAnimation();
+    
+}
+
+
+window.onscroll = scrollFunctions;
