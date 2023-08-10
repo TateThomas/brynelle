1f2af5983f41f8212ffd5fcec4ce065e058c3681
Tate Thomas <106632630+TateThomas@users.noreply.github.com>
2023-08-09 19:53:48 -0600

    Finished pricing page
    Still need to make responsive

---
+++ js/book-now.js
@@ -0,0 +1,22 @@
+let bookNow = `
+            <div>
+                <span></span>
+                <div>
+                    <h1>I can't wait to get to know you!</h1>
+                    <p>lets talk more about your vision</p>
+                </div>
+                <a href="https://brynelle.com/contact">
+                    <div>
+                        <h2>Book Now</h2>
+                    </div>
+                </a>
+            </div>
+            
+            <picture>
+                <source type="image/avif" srcset="./images/goblin_valley_transition.avif">
+                <source type="image/webp" srcset="./images/goblin_valley_transition.webp">
+                <source type="image/jpg" srcset="./images/goblin_valley_transition.jpg">
+                <img src="./images/goblin_valley_transition.avif" alt="Couple kissing in goblin valley" loading="lazy">
+            </picture>
+`;
+document.getElementById("book-now").innerHTML = bookNow;
---
+++ js/steps.js
@@ -0,0 +1,60 @@
+let steps = `
+            <div class="image">
+                <img src="/images/pricing/sun.png">
+            </div>
+            
+            <div class="title">
+                <h2>How it works</h2>
+            </div>
+            
+            <div class="bar">
+            
+                <h2><i>1</i></h2>
+                
+                <h3><i>Fill out the booking inquiry below</i></h3>
+                <p>i'll get back to you within 48 hours</p>
+            
+            </div>
+            
+            <div class="bar">
+            
+                <h2><i>2</i></h2>
+                
+                <h3><i>we'll talk more about your wedding / photoshoot</i></h3>
+                <p>hour coverage, location, travel fees, outfits, etc.</p>
+            
+            </div>
+            
+            <div class="bar">
+            
+                <h2><i>3</i></h2>
+                
+                <h3><i>book your date</i></h3>
+                <p>pay the 30% deposit, answer an optional questionnaire, and sign the contract</p>
+            
+            </div>
+            
+            <div class="bar">
+            
+                <h2><i>4</i></h2>
+                
+                <h3><i>wedding / photoshoot day</i></h3>
+                <p>YAY! the most exciting day! don't stress, i'm here to hype you up and capture these priceless memories for you</p>
+            
+            </div>
+            
+            <div class="bar">
+            
+                <h2><i>5</i></h2>
+                
+                <h3><i>gallery delivered</i></h3>
+                <p>wedding galleries will be delivered within 6 weeks. other sessions will be delivered in 2 weeks</p>
+            
+            </div>
+            
+            <div class="faq">
+                <h2>questions?</h2>
+                <a href="https://brynelle.com/info"><p>click for more info</p></a>
+            </div>
+`;
+document.getElementById("steps").innerHTML = steps;
